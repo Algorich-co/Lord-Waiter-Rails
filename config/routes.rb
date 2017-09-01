0fa230523c7afe93af5c69d007cb2d27664c5184
Taha Rushain <taharushain@gmail.com>
2017-09-01 21:36:18 +0500

    Views - Layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  
+
+  devise_for :restaurant_managers
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root "welcome#about"
+  # root to: 'home#index'
+
   
   get '/signup', to: "registrations#new"
   post '/signup', to: "registrations#create"
@@ -18,4 +21,35 @@ Rails.application.routes.draw do
   
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
+
+  get 'contact', to: 'email_messages#new', as: 'new_email_message'
+  post 'contact', to: 'email_messages#create', as: 'create_message'
+  get 'policy', to: 'home#policy', as: 'policy'
+  get 'eula', to: 'home#eula', as: 'eula'
+
+  get 'restaurant_managers' => 'restaurant_managers#index', :as => "restaurant_managers_index"
+  patch 'restaurant_managers/order_preparing/:order_id' => 'restaurant_managers#order_preparing', :as => 'restaurant_managers_preparing' 
+  patch 'restaurant_managers/order_serving/:order_id' => 'restaurant_managers#order_serving', :as => 'restaurant_managers_serving' 
+  patch 'restaurant_managers/order_checkout/:order_id' => 'restaurant_managers#order_checkout', :as => 'restaurant_managers_checkout' 
+  patch 'restaurant_managers/receive_waiter_call/:waiter_call_id' => 'restaurant_managers#receive_waiter_call', :as => 'restaurant_managers_receive_waiter_call' 
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+
+
+      resources :sessions, only: [:create]
+      post "log_out" => "sessions#destroy", :as => "log_out"
+      resources :clients, only: [:show, :create]
+      post "menu" => "menus#menu", :as => "get_menu"
+      resources :orders, only: [:create]
+      post "facebook_login" => "facebook#create", :as => "facebook_login"
+      post "set_gcm" => "clients#set_gcm", :as => "set_gcm"
+      post "waiter_call" => "orders#waiter_call_action", :as => "waiter_call_action"      
+      post "set_manager_gcm" => "orders#set_manager_gcm", :as => "set_manager_gcm"
+
+
+    end
+  end
+
+
 end
